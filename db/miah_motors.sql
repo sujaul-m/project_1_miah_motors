aef26c59d92262757da85a8e09d54975431341c7
Sujaul Miah <sujaul.m@gmail.com>
2019-10-04 17:37:37 +0100

    created and completed miah_motors.sql

---
+++ db/miah_motors.sql
@@ -0,0 +1,25 @@
+DROP TABLE miah_motors;
+DROP TABLE vehicles;
+DROP TABLE manufacturers;
+
+CREATE TABLE manufacturers
+(
+  id SERIAL8 primary key,
+  name VARCHAR(255) not null,
+  contact VARCHAR(255)
+);
+
+CREATE TABLE vehicles
+(
+  id SERIAL8 primary key,
+  make VARCHAR(255) not null,
+  model VARCHAR(255) not null,
+  selling_price INT8 not null,
+);
+
+CREATE TABLE miah_motors
+(
+  id SERIAL8 primary key,
+  vehicles_id INT8 references vehicles(id),
+  manufacturer_id INT8 references manufacturers(id)
+);
