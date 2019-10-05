2427f5cc480d4bff9d23c1b17d0fc141c0c26c69
Sujaul Miah <sujaul.m@gmail.com>
2019-10-05 16:27:24 +0100

    created view all manufacturers page and it works as expected

---
+++ controllers/manufacturer_controller.rb
@@ -0,0 +1,12 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative( '../models/miah_motors.rb' )
+require_relative( '../models/manufacturer.rb' )
+require_relative( '../models/vehicle.rb' )
+also_reload( '../models/*' )
+
+get "/manufacturers" do
+  @manufacturers = Manufacturer.all()
+  erb ( :"manufacturer/index" )
+end
