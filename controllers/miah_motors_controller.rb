38ab5c69658dec5a7eef7037788f558770a8ebf5
Sujaul Miah <sujaul.m@gmail.com>
2019-10-05 22:31:16 +0100

    added ability to add to inventory list and also delete row using button

--- controllers/miah_motors_controller.rb
+++ controllers/miah_motors_controller.rb
@@ -10,3 +10,20 @@ get "/inventory" do
   @stocks = MiahMotors.all()
   erb ( :"miah_motors/index" )
 end
+
+get '/inventory/new' do
+  @vehicles = Vehicle.all
+  @manufacturers = Manufacturer.all
+  erb(:"miah_motors/new")
+end
+
+post '/inventory' do
+  inventory = MiahMotors.new(params)
+  inventory.save
+  redirect to("/inventory")
+end
+
+post '/inventory/:id/delete' do
+  MiahMotors.delete(params[:id])
+  redirect to("/inventory")
+end
